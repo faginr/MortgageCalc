5b802c2a167241eaa784bf31fcf781c9fccf7f30
faginr <rexwfagin@gmail.com>
2020-01-27 17:47:06 -0800

    Added basic validation.

--- src/com/webdevrex/Main.java
+++ src/com/webdevrex/Main.java
@@ -11,19 +11,34 @@ public class Main {
 
 //      Get Principal from user
         Scanner principalScan = new Scanner(System.in);
-        System.out.print("Principal: ");
+        System.out.println("Principal: ");
         int principal = principalScan.nextInt();
+//        Data validation
+        while (principal < 1000 || principal > 1000000){
+            System.out.println("Enter a value > 1000 and < 1000000: ");
+            principal = principalScan.nextInt();
+        }
+
+
 
 //      Get Interest Rate from user and convert to months
         Scanner rateScan = new Scanner(System.in);
         System.out.print("Annual Interest Rate: ");
         float rate = rateScan.nextFloat();
+        while (rate < 0 || rate > 30){
+            System.out.println("Enter a value > 0 and < 30: ");
+            rate = rateScan.nextFloat();
+        }
         float monthRate = (rate / 100) / 12;
 
 //      Get Period from user and convert to months
         Scanner periodScan = new Scanner(System.in);
         System.out.print("Period (Years): ");
         byte period = periodScan.nextByte();
+        while (period < 1 || period > 30){
+            System.out.println("Enter a value > 0 and < 31: ");
+            period = periodScan.nextByte();
+        }
         int monthPeriod = period * 12;
 
 //      Create numerator and denominator
